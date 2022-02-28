a1f3cddd8cdaac09cdaf09943ec46bc9462ae8e1
Johann Hospice <johann.hospice@externe.sacem.fr>
2022-02-28 02:57:25 +0100

    Rename test description

--- apps/functions/src/strategies/superichimoku/index.test.ts
+++ apps/functions/src/strategies/superichimoku/index.test.ts
@@ -8,7 +8,7 @@ import {
   TIME_PERIOD,
 } from "../../exchanges/binance";
 
-describe("Stratégie Ichimoku", () => {
+describe("Stratégie Super Ichimoku", () => {
   const SYMBOL = "BTCUSDT";
   let klines: CrinKline[];
   let ichimokuStrategy: SuperIchimokuStrategy;

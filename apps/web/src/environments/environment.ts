f39d202a6f1ed8da7a86843966549f952215a161
Johann Hospice <johann.hospice@externe.sacem.fr>
2022-03-09 17:25:05 +0100

    Disable lint

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -1,12 +1,4 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // When building for production, this file is replaced with `environment.prod.ts`.
-const environment = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID,
-};
+const environment = JSON.stringify(process.env["REACT_APP_FIREBASE_CONFIG"]);
 export default environment;

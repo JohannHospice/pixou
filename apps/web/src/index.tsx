7be5886aa9b8eb50a5a8349c4619b2b159c496cf
Johann Hospice <johann.hospice@externe.sacem.fr>
2022-03-09 22:30:04 +0100

    Adding pwa files

--- apps/web/src/index.tsx
+++ apps/web/src/index.tsx
@@ -25,6 +25,9 @@ import AccountPage from "./pages/AccountPage";
 import SettingsPage from "./pages/SettingsPage";
 import HomePage from "./pages/HomePage";
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+
 ReactDOM.render(
   <StrictMode>
     <ThemeProvider theme={getTheme(THEME_MODE)}>
@@ -54,3 +57,13 @@ ReactDOM.render(
   </StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

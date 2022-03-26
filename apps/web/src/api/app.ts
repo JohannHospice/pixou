060627e76d95a1911b97744fd2b2968d776bf7cf
Johann Hospice <johann.hospice@externe.sacem.fr>
2022-03-26 12:57:24 +0100

    Connect functions to emulator if so

--- apps/web/src/api/app.ts
+++ apps/web/src/api/app.ts
@@ -1,6 +1,15 @@
 import { initializeApp } from "firebase/app";
+import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 import firebaseConfig from "../environments/environment";
 
 const app = initializeApp(firebaseConfig);
+
+const hostEmulator = process.env["REACT_APP_FUNCTIONS_EMULATOR_HOST"];
+
+if (hostEmulator) {
+  const [host, port] = hostEmulator.split(":");
+  const functions = getFunctions(app);
+  connectFunctionsEmulator(functions, host, Number(port));
+}
 
 export default app;
